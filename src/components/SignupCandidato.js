6914bd539dc27d98486759c75934e8347bfffeb5
Deat <debora2013aguiartavares@gmail.com>
2021-10-14 21:03:56 -0300

    components candidatos and empresa created

---
+++ src/components/SignupCandidato.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SignupCandidato = () => {
+    return ();
+};
+
+export default SignupCandidato;
---
+++ src/components/SignupEmpresa.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SignupEmpresa = () => {
+    return ();
+};
+
+export default SignupEmpresa;
