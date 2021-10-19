89606f0fb773bf358d5b8c5654025a1f8d24a8d1
Deat <debora2013aguiartavares@gmail.com>
2021-10-19 20:16:50 -0300

    created footer fixed

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,29 @@
 import React from "react";
 
 const Footer = () => {
-    return(
-        <div>
-            footer
-        </div>
-    )
-} 
+  return (
+    <footer className="footer text-center bg-dark text-light p-1">
+      <h5>
+        "Criado por
+        <a
+          class="text-light text-decoration-none"
+          href="https://github.com/jordanavq/"
+        >
+          {" "}
+          Jordana Queiroz{" "}
+        </a>{" "}
+        e
+        <a
+          class="text-light text-decoration-none"
+          href="https://github.com/DeAT1995/"
+        >
+          {" "}
+          Debora Tavares{" "}
+        </a>{" "}
+        como projeto fullstack para escola Ironhack."
+      </h5>
+    </footer>
+  );
+};
 
 export default Footer;
