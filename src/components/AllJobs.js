66c97233a31447846dc011acdf9246722754d347
jordanavq <jordanavq@gmail.com>
2021-10-19 20:49:35 -0300

    allJobs route and jobs cards created

--- src/components/AllJobs.js
+++ src/components/AllJobs.js
@@ -7,7 +7,7 @@ const AllJobs = () => {
   const getAllJobs = async () => {
     try {
       const result = await api.get("/vagas");
-      setJobs([...result.data])
+      setJobs([...result.data]);
       console.log(result);
     } catch (error) {
       console.error(error.response);
@@ -18,19 +18,23 @@ const AllJobs = () => {
   }, []);
 
   return (
-  <div className="p-3"> 
-      {jobs.map(item => (<div className="card" key={item._id}>
-          <h5 className="card-header">
-            {item.empresaId.empresa}
-            {item.data}
-            {item.funcao}
-            {item.cidade}
-          </h5>
-
-      </div>) )}
-  </div>
-  )
+    <div>
+      <h1 className=" p-2 text-primary">VAGAS</h1>
+      <div className="p-3 d-flex">
+        {jobs.map((item) => (
+          <div className="card m-3" key={item._id}>
+            <h4 className="card-header bg-primary text-light">{item.funcao}</h4>
+            <div className="card-body text-secondary">
+              <h5>Empresa: {item.empresaId.empresa}</h5>
+              <h5>Data: {item.data}</h5>
+              <h5>Cidade: {item.cidade}</h5>
+              <h5>Estado: {item.estado}</h5>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
-
 
 export default AllJobs;
