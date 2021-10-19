468a399f0421670fcbd147347eec47a0d880cb2d
Deat <debora2013aguiartavares@gmail.com>
2021-10-19 19:46:03 -0300

    created navbar branch and route fixed

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import SignupCandidato from "./components/SignupCandidato";
 import LoginCandidato from "./components/LoginCandidato";
 import Home from "./components/Home";
 import AllJobs from "./components/AllJobs";
+import NavBar from "./components/NavBar";
 
 
 function App() {
   return (
+    <div>
+    <NavBar/>
     <div className="App">
       <Route exact path="/empresa/cadastro" component={SignupEmpresa} />
       <Route exact path="/empresa/entrar" component={LoginEmpresa} />
@@ -17,7 +20,7 @@ function App() {
       <Route exact path="/candidato/entrar" component={LoginCandidato} />
       <Route exact path="/" component={Home} />
       <Route exact path="/vagas" component={AllJobs} />
-
+      </div>
     </div>
   );
 }
