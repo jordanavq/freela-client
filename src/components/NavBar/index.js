3db49593d4fba8589f35a0e376819fb57944a6d2
Deat <debora2013aguiartavares@gmail.com>
2021-10-19 22:06:32 -0300

    made changes at the navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import logo from "../../images/Logo-Freela.png";
 import { Link } from "react-router-dom";
-import "../NavBar/index.css"
+import "../NavBar/index.css";
 
 const NavBar = () => {
   return (
     <nav className="navbar navbar-light bg-dark w-1000 h-10 m-left-5px">
       <div className="container navbar-fixed-top">
         <Link exact to="/">
-          <img className="logo-navbar" src={logo} alt="logo" width="190" height="50"/>
+          <img
+            className="logo-navbar"
+            src={logo}
+            alt="logo"
+            width="190"
+            height="50"
+          />
         </Link>
       </div>
     </nav>
