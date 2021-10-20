66abee07b2a646f9a7f703f92c6f278f825c30e2
jordanavq <jordanavq@gmail.com>
2021-10-20 20:36:28 -0300

    one job card done

--- src/components/Job.js
+++ src/components/Job.js
@@ -10,6 +10,7 @@ const Job = () => {
   const getJob = async () => {
     try {
       const result = await api.get(`/vagas/${idVaga}`);
+      console.log(result);
       setjob(result.data);
     } catch (error) {
       console.error(error);
@@ -21,7 +22,17 @@ const Job = () => {
   }, []);
   return (
     <div>
-      <h4>{job.funcao}</h4>
+      <h3>Vaga: {job.funcao}</h3>
+      <h3>Empresa: {job.empresaId.empresa}</h3>
+      <h4>Data: {job.data}</h4>
+      <h4>Horário: {job.horario}</h4>
+      <h4>Diária: {job.diaria}</h4>
+      <h4>Traje: {job.traje}</h4>
+      <h4>Fornece refeição: {job.fornece_refeicao}</h4>
+      <h4>Endereço: {job.endereco}</h4>
+      <h4>Cidade: {job.cidade}</h4>
+      <h4>Estado: {job.estado}</h4>
+      <button className="btn btn-light text-secondary">Candidatar-se</button>
     </div>
   );
 };
