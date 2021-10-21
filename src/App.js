ee2bdda5511d6a3d910076a0ffd31a76b75786b6
Deat <debora2013aguiartavares@gmail.com>
2021-10-21 19:34:56 -0300

    created homemain

--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ import SignupEmpresa from "./components/SignupEmpresa";
 import LoginEmpresa from "./components/LoginEmpresa";
 import SignupCandidato from "./components/SignupCandidato";
 import LoginCandidato from "./components/LoginCandidato";
-import Home from "./components/Home";
 import AllJobs from "./components/AllJobs";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import HomeMain from "./components/HomeMain/index.js";
 
 function App() {
   return (
     <div>
       <NavBar />
       <div className="App">
+        <Route exact path="/" component={HomeMain} />
         <Route exact path="/empresa/cadastro" component={SignupEmpresa} />
         <Route exact path="/empresa/entrar" component={LoginEmpresa} />
         <Route exact path="/candidato/cadastro" component={SignupCandidato} />
         <Route exact path="/candidato/entrar" component={LoginCandidato} />
-        <Route exact path="/" component={Home} />
         <Route exact path="/vagas" component={AllJobs} />
       </div>
       <Footer />
