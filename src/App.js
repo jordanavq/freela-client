baf52e369bb1b2b5ca114249f051a87ac468cebd
jordanavq <jordanavq@gmail.com>
2021-10-23 09:37:09 -0300

    update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import SignupEmpresa from "./components/SignupEmpresa";
 import LoginEmpresa from "./components/LoginEmpresa";
 import SignupCandidato from "./components/SignupCandidato";
 import LoginCandidato from "./components/LoginCandidato";
-import Home from "./components/Home";
 import AllJobs from "./components/AllJobs";
 import Job from "./components/Job";
 import AllCandidates from "./components/AllCandidates";
@@ -19,7 +18,6 @@ function App() {
       <Route exact path="/empresa/entrar" component={LoginEmpresa} />
       <Route exact path="/candidato/cadastro" component={SignupCandidato} />
       <Route exact path="/candidato/entrar" component={LoginCandidato} />
-      <Route exact path="/" component={Home} />
       <Route exact path="/vagas" component={AllJobs} />
       <Route exact path="/vagas/:idVaga" component={Job} />
       <Route exact path="/vagas/candidatura/:idVaga" component={Job} />
