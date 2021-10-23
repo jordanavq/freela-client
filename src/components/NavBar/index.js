9c2f68449ce2ded305f179410008388a25e6c831
Deat <debora2013aguiartavares@gmail.com>
2021-10-23 12:11:35 -0300

    made changes at the navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 import "../NavBar/index.css";
 
 const NavBar = () => {
-  return(
+  return (
     <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-  <div className="container-fluid container navbar-fixed-top d-flex justify-content-between mr-0">
-  <Link exact to="/">
+      <div className="container-fluid container navbar-fixed-top d-flex justify-content-between mr-0">
+        <Link exact to="/">
           <img
             className="logo-navbar"
             src={logo}
@@ -16,51 +16,81 @@ const NavBar = () => {
             height="50"
           />
         </Link>
-    
-    <div className="collapse navbar-collapse flex-grow-0" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        
-          
-          <li className="nav-item">
-          <Link exact to="/" style={{textDecoration:"none"}}>
-          <h5 className="nav-link active margin-left" aria-current="page">ENTRAR</h5>
-          </Link>
 
-          </li>
-          <Link exact to="/">
-          <li className="nav-item">
-          <h5 className="nav-link active" aria-current="page" ><div id="style">CADASTRAR</div></h5>
-          </li>
-          </Link>
-        <li className="nav-item dropdown ">
-         <h5 className="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          </h5>
-          <ul className="dropdown-menu" aria-labelledby="navbarDropdown" >
-            <li>
-              <Link exact to="/">
-              <p className="dropdown-item">Action</p>
-              </Link>
-              </li>
-
+        <div
+          className="collapse navbar-collapse flex-grow-0"
+          id="navbarSupportedContent"
+        >
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item dropdown ">
+              <h5
+                className="nav-link dropdown-toggle text-light"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >INFORMAÇÕES</h5>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
               <li>
-              <Link exact to="/">
-              <p className="dropdown-item">Action</p>
-              </Link>
-              </li>
+                  <Link exact to="" style={{ textDecoration: "none" }}>
+                    <p className="dropdown-item">INÍCIO</p>
+                  </Link>
+                </li>
+                <li>
+                ◦ EMPRESA
+                </li>
+                <li>
+                  <Link exact to="/empresa/entrar" style={{ textDecoration: "none" }}>
+                    <p className="dropdown-item">Entrar</p>
+                  </Link>
+                </li>
 
-              <li>
-              <Link exact to="/">
-              <p className="dropdown-item">Action</p>
-              </Link>
-              </li>
+                <li>
+                  <Link exact to="/empresa/cadastro" style={{ textDecoration: "none" }}>
+                    <p className="dropdown-item">Cadastrar</p>
+                  </Link>
+                </li>
+
+                <li>
+                  <Link exact to="/candidatos" style={{ textDecoration: "none" }}>
+                    <p className="dropdown-item">Ver Candidatos</p>
+                  </Link>
+                </li>
+
+                <li>
+                ◦ CANDIDATO
+                </li>
+                <li>
+                  <Link exact to="/candidato/entrar" style={{ textDecoration: "none" }}>
+                    <p className="dropdown-item">Entrar</p>
+                  </Link>
+                </li>
+
+                <li>
+                  <Link exact to="/candidato/cadastro" style={{ textDecoration: "none" }}>
+                    <p className="dropdown-item">Cadastrar</p>
+                  </Link>
+                </li>
+
+                <li>
+                  <Link exact to="/vagas" style={{ textDecoration: "none" }}>
+                    <p className="dropdown-item">Ver Vagas</p>
+                  </Link>
+                </li>
+                <li>
+                  <Link exact to="/sair" style={{ textDecoration: "none" }}>
+                    <p className="dropdown-item">Sair da conta</p>
+                  </Link>
+                </li>
+              </ul>
+            </li>
           </ul>
-        </li>
-      </ul>
-      
-    </div>
-  </div>
-</nav>
-  )
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
+
